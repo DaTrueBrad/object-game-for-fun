6dcbc362b70384a3ede152f1073fff4e28e5d4a3
Brady Bott <bottbrady@gmail.com>
2021-07-16 14:28:04 -0600

    Stuart helped me get it working!

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ class Char {
     }
     attackRange(otherChar){
       if(this.rangeMove >= otherChar.health){
-        otherchar.health = 0;
+        otherChar.health = 0;
         console.log(`${this.name} totally just killed ${otherChar.name}! End of Match!`)
       } else {
         otherChar.health -= this.rangeMove;
@@ -26,15 +26,16 @@ class Char {
       }
     }
   }
+const { rawListeners } = require('process')
   const readline = require('readline')
   
   let rick = new Char(`Rick`, `Scientist`, 80, 0, 7, 18)
   let sasuke = new Char(`Sasuke`, `Rogue Shinobi`, 140, 0, 12, 12)
   
-  console.log(rick)
-  console.log(sasuke)
+  // console.log(rick)
+  // console.log(sasuke)
   
-  rick.attackMelee(sasuke)
+  // rick.attackMelee(sasuke)
   
   //Line 29 and the code below needs to be adapted for user input for attack. Loop through both rick and sasuke until one of them is dead.//
   
@@ -43,11 +44,81 @@ class Char {
       output: process.stdout
     })
   
-  reader.question("Who's turn is it now?", function(num){
-      if (+num === 12){
-          console.log("That's Adam's favorite!")
+function askQuestion(string, character){
+  return new Promise(resolve => reader.question(string, function(answer){
+    if (answer === `melee`){
+      if(character === "Rick"){
+        rick.attackMelee(sasuke)
+        console.log("Rick hit Sasuke with his car!")
+        // console.log(`Sasuke's health is now ${sasuke.health}`)
       } else {
-          console.log("Womp womp thanks for playing")
+        sasuke.attackMelee(rick)
+        console.log("Sasuke punched rick!")
       }
-      reader.close();
-  })
+
+    } else if(answer === `range`) {
+        if(character === "Rick"){
+          rick.attackRange(sasuke)
+          console.log("Rick lasered Sasuke!")
+          // console.log(`Sasuke's health is now ${sasuke.health}`)
+        } else {
+          sasuke.attackRange(rick)
+          console.log("Sasuke fireballed Rick!")
+          
+      }
+    }
+    // reader.close()
+    resolve()
+  }))
+}
+
+  // reader.question("Who's turn is it now?", function(num){
+  //     if (+num === 12){
+  //         console.log("That's Adam's favorite!")
+  //     } else {
+  //         console.log("Womp womp thanks for playing")
+  //     }
+  //     reader.close();
+  // })
+async function startFight(){
+  for(let i = 0; i < 10000; i++){
+    console.log(sasuke)
+    console.log(rick)
+    if(sasuke.health <= 0 || rick.health <= 0){
+      console.log(`Match Over!`)
+      reader.close()
+      break;
+    }
+    async function rickMove(){
+      await askQuestion("Rick, what move would you like: Melee or Range?","Rick")
+      // reader.question("Rick, what move would you like: Melee or Range?", function(answer){
+      //   if (answer == `melee`){
+      //     rick.attackMelee(sasuke)
+      //       // console.log("Sasuke used Lion's Barrage on Rick!")
+      //       // console.log(`Rick's health is now ${rick.health}`)
+      //   } else if(answer == `range`) {
+      //       rick.attackRange(sasuke)
+      //       // console.log("Sasuke cast fireball-jutsu on Rick!")
+      //       // console.log(`Rick's health is now ${rick.health}`)
+      //   }
+      // })
+    }
+    await rickMove()
+    async function sasukeMove(){
+      await askQuestion("Sasuke, what move would you like: Melee or Range?","Sasuke")
+      // reader.question("Sasuke, what move would you like: Melee or Range?", function(answer){
+      //   if (answer == `melee`){
+      //     sasuke.attackMelee(rick)
+      //       // console.log("Sasuke used Lion's Barrage on Rick!")
+      //       // console.log(`Rick's health is now ${rick.health}`)
+      //   } else if(answer == `range`) {
+      //       sasuke.attackRange(rick)
+      //       // console.log("Sasuke cast fireball-jutsu on Rick!")
+      //       // console.log(`Rick's health is now ${rick.health}`)
+      //   }
+      // })
+    }
+   await sasukeMove()
+  }
+}
+startFight()
